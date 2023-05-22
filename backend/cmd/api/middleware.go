067f604325eab452d6ae8f5bc0cc533ba5605ac5
JasonRay168 <e0985893@u.nus.edu>
2023-05-22 19:15:03 +0800

    Able to fetch json from backend and display data in frontend

--- backend/cmd/api/middleware.go
+++ backend/cmd/api/middleware.go
@@ -5,7 +5,7 @@ import "net/http"
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	origin := "https://nautilus-delta.vercel.app/"
 	if app.config.localCORS {
-		origin = "localhost:3000"
+		origin = "http://localhost:3000"
 	}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", origin)
