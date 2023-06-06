e64b5924dcea553cd141cd306af031825ec5b660
Miffi <miffi@decadent.rest>
2023-06-06 14:28:01 +0600

    feat(backend): Add panic recovery
    Current panics in the backend return a http 52 empty return call to the
    client. This commit adds a middleware that tells the client that this is
    a server error.

--- backend/cmd/api/middleware.go
+++ backend/cmd/api/middleware.go
@@ -16,3 +16,20 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a defer function, which will always run regardless of there
+		// being a panic or not.
+
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
