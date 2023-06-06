684855264332585c965fa45fe823983ba5de9b59
JasonRay168 <e0985893@u.nus.edu>
2023-06-07 00:02:24 +0800

    Minor design improvements, moved graph data and functions up the component tree (from Graph to App), implement a drop-down menu and a button that display in console selected options

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 
 import Sidebar from './components/Sidebar/Sidebar';
@@ -6,10 +6,110 @@ import Graph from './components/Graph/Graph';
 
 
 function App() {
+  const graphURI = process.env.REACT_APP_BACKEND_HOSTNAME + "/v1/fullGraph.json"
+
+  const [toggleFilter, setToggleFilter] = useState(false);
+  const [toggleDesc, setToggleDesc] = useState(false);
+
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const [xCoor, setXCoor] = useState(0);
+  const [yCoor, setYCoor] = useState(0);
+  const graphRef = useRef();
+
+  function closeFilter() {
+    maximizeFilter();
+    setToggleFilter(false);
+    document.getElementById('filter').style.left = '-40%';
+
+    graphRef.current.centerAt(xCoor + 20, yCoor, 200)
+    setXCoor(xCoor + 20);
+  }
+  function openFilter() {
+      if (toggleDesc) {
+          minimizeFilter();
+      }
+      setToggleFilter(true);
+      document.getElementById('filter').style.left = '100px';
+
+      graphRef.current.centerAt(xCoor - 20, yCoor, 200)
+      setXCoor(xCoor - 20);
+  }
+  function closeDesc() {
+      if (toggleFilter) {
+          maximizeFilter();
+      }
+      setToggleDesc(false);
+      document.getElementById('description').style.bottom = '-42%';
+  }
+  function openDesc(text) {
+      if (toggleFilter) {
+          minimizeFilter();
+      }
+      setToggleDesc(true);
+      document.getElementById('description').style.bottom = '0';
+      document.getElementById('description_content').innerHTML = text;
+  }
+  function minimizeFilter() {
+      document.getElementById('filter').style.height = '60%';
+  }
+
+  function maximizeFilter() {
+      document.getElementById('filter').style.height = '100%';
+  }
+
+  useEffect(() => {
+    fetch(graphURI)
+    .then(response => {
+        if (response.ok){
+            return response.json();
+        }
+        throw response;
+    })
+    .then(data => {
+        setData(data);
+    })
+    .catch(error => {
+        console.error("Error fetching graph data: ", error);
+        setError(error);
+    })
+    .finally(() => {
+        setLoading(false);
+    })
+}, [graphURI]);
+
+  let props = {
+    graphURI: graphURI,
+    graphData: data,
+    error: error,
+    loading: loading,
+    setData: setData,
+    setError: setError,
+    setLoading: setLoading,
+
+    toggleDesc: toggleDesc,
+    toggleFilter: toggleFilter,
+    setToggleDesc: setToggleDesc,
+    setToggleFilter: setToggleFilter,
+    closeFilter: closeFilter,
+    openFilter: openFilter,
+    closeDesc: closeDesc,
+    openDesc: openDesc,
+    maximizeFilter: maximizeFilter,
+    minimizeFilter: minimizeFilter,
+
+    graphRef: graphRef,
+    xCoor: xCoor,
+    yCoor: yCoor,
+    setYCoor: setYCoor,
+    setXCoor: setXCoor
+  }
   return (
     <div className='App'>
-      <Sidebar />
-      <Graph />
+      <Sidebar {...props}/>
+      <Graph {...props}/>
     </div>
   )
 }
--- frontend/src/components/Sidebar/components/Filter/Filter.jsx
+++ frontend/src/components/Sidebar/components/Filter/Filter.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './filter.css';
 
-function Filter() {
+function Filter(props) {
+    function Form() {
+    
+        function handleSubmit() {
+            let group = document.getElementById("semester_dropdown").value;
+            group === "none"
+                ? console.log("No Filter")
+                : console.log("Group         " + group);
+        }
+    
+        return (
+            <div className="filter-form" id="filter_form">
+                <label htmlFor='semester_dropdown' style={{paddingLeft: 2 + 'px'}}>Semester</label><br />
+                <select name="semester" id="semester_dropdown" className="semester">
+                    <option value="none"></option>
+                    <option value="0">Group 0</option>
+                    <option value="1">Group 1</option>
+                    <option value="2">Group 2</option>
+                </select><br />
+                <br />
+                <button value="Apply" onClick={handleSubmit}>Apply</button>
+            </div>
+        );
+    }
+
+    const [isChecked, setIsChecked] = useState(false);
+
     return (
         <div className="menu-filter" id="filter">
             <h1>Filter</h1>
-            <p>whatever will be inside the filter</p>
-            <p>whatever will be inside the filter</p>
-            <p>whatever will be inside the filter</p>
-            <p>whatever will be inside the filter</p>
-            <p>whatever will be inside the filter</p>
-            <p>whatever will be inside the filter</p>
-            <p>whatever will be inside the filter</p>
-            <p>whatever will be inside the filter</p>
-            <p>whatever will be inside the filter</p>
-            <p>whatever will be inside the filter</p>
-            <p>whatever will be inside the filter</p>
+            <div className="filter-toggle">
+                <input type="checkbox" id="open_filter" value="Open Filter" onChange={() => setIsChecked(!isChecked)} checked={isChecked}/>
+                <label htmlFor="open_filter">Open Filter</label>
+            </div>
+            <br />
+            {isChecked
+            ? <Form />
+            : <p>Check the box to open the filter!</p>}
+            <div id="answer"></div>
         </div>
     );
 }
