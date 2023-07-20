a3818e4b22b3f95cd832f16882fce8ae664c6b41
JasonRay168 <e0985893@u.nus.edu>
2023-07-20 22:34:16 +0800

    Limit courses inside search bar suggestions to the current graph

--- frontend/src/components/Sidebar/components/Filter/Filter.jsx
+++ frontend/src/components/Sidebar/components/Filter/Filter.jsx
@@ -64,14 +64,29 @@ function Filter(props) {
 			// console.log(data);
 			if (!data.nodes) window.alert('No courses fit this description!')
 			else if (data.nodes && !data.links) {
+				console.log(data);
 				data.links = [];
+				updateNodeSet(prevNodeSet => {
+					prevNodeSet.clear();
+					data.nodes.forEach(node => prevNodeSet.add(node.id));
+					console.log(prevNodeSet);
+					return prevNodeSet;
+				})
+				updateLinkSet(prevLinkSet => {
+					prevLinkSet.clear();
+					data.links.forEach(link => prevLinkSet.add(JSON.stringify(link)));
+					// console.log(prevLinkSet);
+					return prevLinkSet;
+				})
 				setGraphData(() => data)
 				//console.log(data)
+				graphRef.current.zoom(2);
+				graphRef.current.centerAt(-20, 0, 200);
 			} else {
 				updateNodeSet(prevNodeSet => {
 					prevNodeSet.clear();
 					data.nodes.forEach(node => prevNodeSet.add(node.id));
-					// console.log(prevNodeSet);
+					console.log(prevNodeSet);
 					return prevNodeSet;
 				})
 				updateLinkSet(prevLinkSet => {
