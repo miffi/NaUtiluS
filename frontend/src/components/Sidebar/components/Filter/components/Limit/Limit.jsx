043e48a0a26a275b5f53af00d179bfc48b86294b
JasonRay168 <e0985893@u.nus.edu>
2023-08-20 16:12:59 +0800

    Added default value for limit

--- frontend/src/components/Sidebar/components/Filter/components/Limit/Limit.jsx
+++ frontend/src/components/Sidebar/components/Filter/components/Limit/Limit.jsx
@@ -5,7 +5,7 @@ function Limit() {
 	return (
 		<div id='limit-container' className='limit-container'>
 			<label className='limit-label'>Limit depth to:</label>
-			<input id='limit-input' className='limit-input' type="text" placeholder='Enter a limit' autoComplete='off'/>
+			<input id='limit-input' className='limit-input' type="text" placeholder='Enter a limit' defaultValue='2' autoComplete='off'/>
 		</div>
 	)
 }
